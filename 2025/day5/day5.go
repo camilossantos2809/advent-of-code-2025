d64f535a45c7c82be4feebd592c99f164b0dbd11
camilossantos2809 <camilossantos.2809@gmail.com>
2025-12-15 19:37:18 -0300

    refactor

--- 2025/day5/day5.go
+++ 2025/day5/day5.go
@@ -13,37 +13,62 @@ type Range struct {
 	end   int
 }
 
-func parseRanges(lines []string) (ranges []Range, codes []int) {
+func parseRange(line string) (Range, error) {
+	strValues := strings.Split(line, "-")
+	start, err := strconv.Atoi(strValues[0])
+	if err != nil {
+		return Range{}, fmt.Errorf("failed to convert start value %s", strValues[0])
+	}
+
+	end, err := strconv.Atoi(strValues[1])
+	if err != nil {
+		return Range{}, fmt.Errorf("failed to convert end value %s", strValues[0])
+	}
+
+	return Range{start, end}, nil
+}
+
+func parseCode(line string) (int, error) {
+	code, err := strconv.Atoi(line)
+	if err != nil {
+		log.Fatalf("Failed to convert end value %s", line)
+	}
+
+	return code, nil
+}
+
+func parseRanges(lines []string) ([]Range, []int, error) {
+	var ranges []Range
+	var codes []int
+
 	for _, line := range lines {
 		if line == "" {
 			continue
 		}
-
 		if strings.Contains(line, "-") {
-			strValues := strings.Split(line, "-")
-			start, err := strconv.Atoi(strValues[0])
+			parsed, err := parseRange(line)
 			if err != nil {
-				log.Fatalf("Failed to convert start value %s", strValues[0])
+				return nil, nil, fmt.Errorf("failed to parse range")
 			}
-			end, err := strconv.Atoi(strValues[1])
-			if err != nil {
-				log.Fatalf("Failed to convert end value %s", strValues[0])
-			}
-
-			ranges = append(ranges, Range{start, end})
+			ranges = append(ranges, parsed)
 		} else {
-			code, err := strconv.Atoi(line)
+			code, err := parseCode(line)
 			if err != nil {
-				log.Fatalf("Failed to convert end value %s", line)
+				return nil, nil, fmt.Errorf("failed to parse code %s", line)
 			}
 			codes = append(codes, code)
 		}
 	}
-	return ranges, codes
+
+	return ranges, codes, nil
 }
 
 func countFreshIngredients(lines []string) int {
-	ranges, codes := parseRanges(lines)
+	ranges, codes, err := parseRanges(lines)
+	if err != nil {
+		log.Fatal("Failed to parse ranges")
+	}
+
 	var freshCount int
 	for _, code := range codes {
 		for _, _range := range ranges {
