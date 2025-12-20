24d938bafe6c736109414701b3638d002b506315
camilossantos2809 <camilossantos.2809@gmail.com>
2025-12-20 18:09:21 -0300

    first try to part 2

--- 2025/day6/day6.go
+++ 2025/day6/day6.go
@@ -30,7 +30,7 @@ func (p *Problem) calcTotal() {
 	}
 }
 
-func calcGrandTotal(lines []string) int {
+func calcGrandTotalPart1(lines []string) int {
 	problems := make(map[int]Problem)
 	for _, line := range lines {
 		lineSlice := strings.Split(line, " ")
@@ -70,9 +70,47 @@ func calcGrandTotal(lines []string) int {
 	return total
 }
 
+func calcGrandTotalPart2(lines []string) int {
+	var problems []Problem
+	var values []int
+	for i := len(lines[0]) - 1; i >= 0; i-- {
+
+		if (i+1)%4 == 0 {
+			problems = append(problems, Problem{values: values, operation: string(lines[3][i+1])})
+			values = make([]int, 0)
+			continue
+		}
+		var builder strings.Builder
+		builder.WriteByte(lines[0][i])
+		builder.WriteByte(lines[1][i])
+		builder.WriteByte(lines[2][i])
+		numStr := strings.TrimSpace(builder.String())
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			log.Fatalf("Failed to convert string %s", numStr)
+		}
+		values = append(values, num)
+
+		if i == 0 {
+			problems = append(problems, Problem{values: values, operation: string(lines[3][i+1])})
+			values = make([]int, 0)
+		}
+	}
+
+	var total int
+	for _, problem := range problems {
+		problem.calcTotal()
+		total += problem.total
+	}
+	return total
+}
+
 func Run() {
 	lines := helpers.ReadInput("2025/day6/input.txt")
 
-	part1 := calcGrandTotal(lines)
+	part1 := calcGrandTotalPart1(lines)
 	fmt.Println("Part 1", part1) // 4771265398012
+
+	part2 := calcGrandTotalPart2(lines)
+	fmt.Println("Part 1", part2)
 }
--- 2025/day6/day6_test.go
+++ 2025/day6/day6_test.go
@@ -10,9 +10,49 @@ func TestPart1(test *testing.T) {
 			"  6 98  215 314",
 			"*   +   *   +  ",
 		}
-		result := calcGrandTotal(lines)
+		result := calcGrandTotalPart1(lines)
 		// 33210 + 490 + 4243455 + 401
 		expected := 4277556
+		if result != expected {
+			test.Errorf("result = %d; expected %d", result, expected)
+		}
+	})
+}
+
+func TestPart2(test *testing.T) {
+	test.Run("Deve retornar o valor correto para o exemplo fornecido no site", func(test *testing.T) {
+		lines := []string{
+			"123 328  51 64 ",
+			" 45 64  387 23 ",
+			"  6 98  215 314",
+			"*   +   *   +  ",
+		}
+		result := calcGrandTotalPart2(lines)
+		// The rightmost problem is 4 + 431 + 623 = 1058
+		// The second problem from the right is 175 * 581 * 32 = 3253600
+		// The third problem from the right is 8 + 248 + 369 = 625
+		// Finally, the leftmost problem is 356 * 24 * 1 = 8544
+		// Now, the grand total is 1058 + 3253600 + 625 + 8544 = 3263827.
+		expected := 3263827
+		if result != expected {
+			test.Errorf("result = %d; expected %d", result, expected)
+		}
+	})
+
+	test.Run("Deve considerar colunas com quantidades de dígitos diferentes", func(test *testing.T) {
+		lines := []string{
+			"264 421 92 23",
+			"136 212 97 54",
+			" 37 416 89 6 ",
+			"+   +   +  + ",
+		}
+		result := calcGrandTotalPart2(lines)
+		// 34 + 256 = 290
+		// 279 + 998 = 1277
+		// 126 + 211 + 424 = 761
+		// 467 + 633 + 21 = 1121
+		// 290 + 1277 + 761 + 1121 = 3449
+		expected := 3449
 		if result != expected {
 			test.Errorf("result = %d; expected %d", result, expected)
 		}
