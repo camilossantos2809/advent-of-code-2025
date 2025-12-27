bd25547babc0cb1fab0a3a41cd624ace6a05c695
camilossantos2809 <camilossantos.2809@gmail.com>
2025-12-27 11:23:39 -0300

    calculate distance

--- 2025/day8/day8.go
+++ 2025/day8/day8.go
@@ -3,10 +3,68 @@ package day8
 import (
 	"advent-of-code/helpers"
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 )
 
+type Point struct {
+	x, y, z int
+}
+
+func (p *Point) NewPoint(x, y, z string) {
+	numx, err := strconv.Atoi(x)
+	if err != nil {
+		log.Fatalf("Failed to convert x")
+	}
+	numy, err := strconv.Atoi(y)
+	if err != nil {
+		log.Fatalf("Failed to convert y")
+	}
+	numz, err := strconv.Atoi(z)
+	if err != nil {
+		log.Fatalf("Failed to convert z")
+	}
+
+	p.x = numx
+	p.y = numy
+	p.z = numz
+}
+
+type Connection struct {
+	firstPoint, secondPoint, distance int
+}
+
+func (c *Connection) newConnection(firstPoint, secondPoint Point, firstI, secondI int) {
+	c.firstPoint = firstI
+	c.secondPoint = secondI
+
+	dx := secondPoint.x - firstPoint.x
+	dy := secondPoint.y - firstPoint.y
+	dz := secondPoint.z - firstPoint.z
+	c.distance = dx*dx + dy*dy + dz*dz
+}
+
 func multiply3LargestCircuits(lines []string) int {
-	return len(lines)
+	var points []Point
+	for _, line := range lines {
+		split := strings.Split(line, ",")
+		point := Point{}
+		point.NewPoint(split[0], split[1], split[2])
+		points = append(points, point)
+	}
+
+	var connections []Connection
+	for i, parentPoint := range points {
+		for j := i + 1; j < len(points); j++ {
+			connection := Connection{}
+			connection.newConnection(parentPoint, points[j], i, j)
+			connections = append(connections, connection)
+		}
+
+	}
+
+	return len(connections)
 }
 
 func Run() {
